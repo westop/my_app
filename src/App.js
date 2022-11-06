07e80571e803b20ab4c4b653ce4d5192f24723b2
westop <85644177+westop@users.noreply.github.com>
2022-11-06 21:10:07 +0200

    изменено:      src/App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-
+import React, { useState } from 'react'
 
 function App() {
+
+
+const [likes, setlikes] = useState(0)
+
+function increment() {
+  setlikes(likes +1);
+}
+
+function decrement() {
+  setlikes(likes -1);
+}
+
+
+
+
+
   return (
     <div className="App">
-      wgwf
+      <h1>{likes}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
   );
 }
