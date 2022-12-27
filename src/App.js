3d423a72d3094032d6ed7f9b8958e3e7b154d3e2
westop <dimitriy20122012@gmail.com>
2022-12-27 21:38:36 +0200

    изменено:      package-lock.json 	изменено:      package.json 	изменено:      src/API/PostService.js 	изменено:      src/App.js 	изменено:      src/style/App.css 	новый файл:    src/utils/pages.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import axios from 'axios';
 import PostService from './API/PostService';
 import Loader from './Components/UI/Loader/Loader';
 import { useFetching } from './Components/Hooks/useFetching';
+import { getPageCount, getPagesArray } from './utils/pages';
 
 
 
@@ -25,10 +26,17 @@ function App() {
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   const sortedAndSeachedPosts = usePosts(posts, filter.sort, filter.query);
+  let pagesArray = getPagesArray(totalPages);
+
   const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const response = await PostService.getAll();
+    const response = await PostService.getAll(limit, page);
     setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit));
   })
 
   useEffect(() => {
@@ -47,24 +55,36 @@ function App() {
 
   return (
     <div className='App'>
+
       <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
         Create user
       </MyButton>
+
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
+
       <hr style={{ margin: '15px 0' }}></hr>
+
       <PostFilter
         filter={filter}
         setFilter={setFilter}
       />
+
       {postError &&
-      <h1>Error ${postError}</h1>
+        <h1>Error ${postError}</h1>
       }
+
       {isPostsLoading
         ? <div style={{ display: 'flex', justifyContent: 'center', matginTop: 50 }}><Loader /></div>
         : <PostList remove={removePost} posts={sortedAndSeachedPosts} title="Post list 1" />
       }
+
+      <div className='page__wrapper'>
+        {pagesArray.map(p =>
+          <span className='page'>{p}</span>
+        )}
+      </div>
 
     </div>
   );
